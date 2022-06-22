6062e0b1d481a77ceb0f5c700024526042e98884
nenadvico <vico.nenad@gmail.com>
2022-06-22 22:10:49 +0200

    Fix for issue #309 Unable to configure flyway.datasources.*.ignore-migration-patterns

--- flyway/src/test/groovy/io/micronaut/flyway/FlywayConfigurationPropertiesEnabledSpec.groovy
+++ flyway/src/test/groovy/io/micronaut/flyway/FlywayConfigurationPropertiesEnabledSpec.groovy
@@ -3,6 +3,7 @@ package io.micronaut.flyway
 import io.micronaut.context.exceptions.NoSuchBeanException
 import io.micronaut.inject.qualifiers.Qualifiers
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.pattern.ValidatePattern
 
 import javax.sql.DataSource
 
@@ -57,6 +58,29 @@ class FlywayConfigurationPropertiesEnabledSpec extends AbstractFlywaySpec {
 
         then:
         noExceptionThrown()
+    }
+
+    void 'if flyway configuration then ValidatePatternTypeConverter and Flyway beans are created'() {
+        given:
+        run('spec.name'                         : FlywayConfigurationPropertiesEnabledSpec.simpleName,
+                'flyway.datasources.movies.enabled' : true,
+                'flyway.datasources.movies.ignore-migration-patterns' : "*:missing",
+                'datasources.movies.url'            : 'jdbc:h2:mem:flyway2Db;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE',
+                'datasources.movies.username'       : DS_USERNAME,
+                'datasources.movies.password'       : DS_PASSWORD,
+                'datasources.movies.driverClassName': DS_DRIVER)
+
+        when:
+        applicationContext.getBean(DataSource, Qualifiers.byName('movies'))
+        applicationContext.getBean(FlywayConfigurationProperties, Qualifiers.byName('movies'))
+        applicationContext.getBean(Flyway, Qualifiers.byName('movies'))
+
+        then:
+        noExceptionThrown()
+
+        expect:
+        applicationContext.getConversionService().canConvert(String.class, ValidatePattern.class)
+
     }
 
     void 'if flyway is disabled, then FlywayConfigurationProperties bean is created but Flyway bean is not'() {
---
+++ flyway/src/test/groovy/io/micronaut/flyway/ValidatePatternTypeConverterSpec.groovy
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.flyway
+
+import io.micronaut.core.convert.ConversionContext
+import io.micronaut.core.convert.ConversionService
+import io.micronaut.core.convert.DefaultConversionService
+import io.micronaut.core.convert.exceptions.ConversionErrorException
+import io.micronaut.core.type.Argument
+import org.flywaydb.core.api.ErrorCode
+import org.flywaydb.core.api.FlywayException
+import org.flywaydb.core.api.MigrationState
+import org.flywaydb.core.api.pattern.ValidatePattern
+import org.flywaydb.core.internal.license.FlywayTeamsUpgradeRequiredException
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.nio.charset.Charset
+import java.time.DayOfWeek
+
+/**
+ * Created by graemerocher on 12/06/2017.
+ */
+class ValidatePatternTypeConverterSpec extends Specification {
+
+
+    void "test empty string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        expect:
+        !validatePatternTypeConverter.convert("", ValidatePattern.class).isPresent()
+    }
+
+    void "test wrong string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        when:
+        validatePatternTypeConverter.convert("junk", ValidatePattern.class)
+
+        then:
+        def e = thrown(FlywayException)
+        e.errorCode == ErrorCode.ERROR
+        e.message == 'Invalid pattern \'junk\'. Pattern must be of the form <migration_type>:<migration_state> See https://rd.gt/37m4hXD for full details'
+    }
+
+    void "test future string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        expect:
+        validatePatternTypeConverter.convert("*:future", ValidatePattern.class).get()
+                .matchesMigration(false, MigrationState.FUTURE_SUCCESS)
+    }
+
+    void "test repeatable string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        when:
+        validatePatternTypeConverter.convert("repeatable:*", ValidatePattern.class)
+
+        then:
+        def e = thrown(FlywayTeamsUpgradeRequiredException)
+        e.errorCode == ErrorCode.ERROR
+        e.message == 'Flyway Teams Edition upgrade required: ignoreMigrationPattern with type \'repeatable\' is not supported by Flyway Community Edition\n' +
+                'Try Flyway Teams Edition for free: https://rd.gt/2VzHpkY'
+    }
+
+    void "test versioned string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        when:
+        validatePatternTypeConverter.convert("versioned:missing", ValidatePattern.class)
+
+        then:
+        def e = thrown(FlywayTeamsUpgradeRequiredException)
+        e.errorCode == ErrorCode.ERROR
+        e.message == 'Flyway Teams Edition upgrade required: ignoreMigrationPattern with type \'versioned\' is not supported by Flyway Community Edition\n' +
+                'Try Flyway Teams Edition for free: https://rd.gt/2VzHpkY'
+    }
+
+    void "test missing string conversion"() {
+        given:
+        ValidatePatternTypeConverter validatePatternTypeConverter = new ValidatePatternTypeConverter()
+
+        expect:
+        validatePatternTypeConverter.convert("*:missing", ValidatePattern.class).get()
+                .matchesMigration(false, MigrationState.MISSING_SUCCESS)
+    }
+
+}
