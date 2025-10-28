9d9ce05b7280fc8b09db961b2e7d6cffb2c8000a
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-28 10:46:58 +0100

    fix flyway-tests

--- test-suite-serde/build.gradle.kts
+++ test-suite-serde/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 dependencies {
-    testAnnotationProcessor(mn.micronaut.inject.java)
+
     testAnnotationProcessor(mnSerde.micronaut.serde.processor)
     testImplementation(mnSerde.micronaut.serde.jackson)
-    testImplementation(mnTest.micronaut.test.junit5)
+
     testImplementation(mn.micronaut.http.server.netty)
     testImplementation(mn.micronaut.http.client)
     testImplementation(projects.micronautFlyway)
     testRuntimeOnly(mnSql.h2)
     testRuntimeOnly(mnSql.micronaut.jdbc.hikari)
-    testRuntimeOnly(mnLogging.logback.classic)
+
 }
